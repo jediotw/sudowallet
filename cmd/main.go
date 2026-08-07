7e9bac8db998a8c4db10a926458d09ed7fe06a22
saurabhkr78 <meetsaurabh78@gmail.com>
2026-08-07 14:40:21 +0530

    init setup ..

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package cmd
+
+import ()
+
+func main() {
+	//1. load the config
+	//2. infuse the config in database connection
+	//3. start the server
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module sudowallet
+
+go 1.26.5
